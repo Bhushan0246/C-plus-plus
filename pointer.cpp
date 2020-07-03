2abcd83c0dbd61c74ddc428e248f9380b69f5f8b
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2020-07-03 19:26:20 +0530

    modified

--- pointer.cpp
+++ pointer.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int  main() 
 {
-	int a,*p;
-	cout<<"Enter a number: ";
-	cin>>a;
+	int a,*p,**n;
+	cout<<"here is a variable a";
 	p=&a;
 	cout<<endl<<"The address of variable a is: "<<p;
+	*n=&*p;
+	cout<<endl<<"The address of pointer p is: "<<n;
+	return 0;
 }
