ed8c5cbc669205025ecb52eb25ff949d4766c0a4
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2020-07-04 14:40:41 +0530

    new file

---
+++ km_to_m.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float km;
+	cout<<"\tConvert kilometre to metres"<<endl;
+	cout<<"Enter the distance in km: ";
+	cin>>km;
+	cout<<"The distance in m: "<<km*1000;
+	return 0;
+}
