0249db3c832324ae9cd5a4727cc7bf39bb363e1e
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2020-07-05 15:40:44 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c;
+	cout<<"\tSwapping of numbers"<<endl;
+	cout<<"Enter two numbers for a and b: ";
+	cin>>a>>b;
+	c=a;
+	a=b;
+	b=c;
+	cout<<"Now, the values after swapping are"<<endl;
+	cout<<"a: "<<a<<"\tb: "<<b;
+	return 0;
+}
