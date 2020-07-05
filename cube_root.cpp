4fcfdff4f61d82c8e5f1973bb1b049c4bda1019b
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2020-07-05 15:39:17 +0530

    Add files via upload

---
+++ cube_root.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+float n;
+cout<<"\tFind Cube Root"<<endl;
+cout<<"Enter a number to find cube root from: ";
+cin>>n;
+while(n<=20){
+cout<<"The cube root of "<<n<<" is: "<<cbrt(n)<<endl;
+n++;
+}
+return 0;
+}
+
