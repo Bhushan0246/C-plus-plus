dc356edd2c28fb24a3b4b970495586c837223626
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2020-07-13 15:10:30 +0530

    Add files via upload
    You have to guess my lucky number.

---
+++ guess_number.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int n,tries=1;
+cout<<"Hello Friend!\nThis is the time to have some fun"<<endl;
+cout<<"I have a Lucky Number guess it: "<<endl;
+cin>>n;
+while(n!=7 && tries<=3) {
+	system("CLS");
+	cout<<"OH! Wrong guess...\nTries: "<<tries<<endl<<"Try again"<<endl;
+	tries++;
+	cout<<"I have a Lucky Number guess it: "<<endl;
+	cin>>n;
+}
+if(n==7) {
+	cout<<"Yeah! Right guess.\nCongratulations";
+}
+	if(tries<=3) {
+		cout<<"OH! You reached Tries limit: "<<tries<<endl;
+	}
+return 0;
+}
+
