1b49eed3e293af876f6f7146c570224c7f138064
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2020-07-19 13:11:04 +0530

    Add files via upload

---
+++ bool.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+bool a=false,b=true;
+cout<<"The value of a is False,therefore: "<<a<<endl;
+cout<<"The value of b is True,therefore: "<<b<<endl;
+return 0;
+}
+
