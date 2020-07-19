e3c438088b3370e55871efc2e1c577a67ed746a4
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2020-07-19 12:51:31 +0530

    Modified
    How to modify Pointers value.

--- pointer.cpp
+++ pointer.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int  main() 
 {
-	int a,*p,**n;
-	cout<<"here is a variable a";
+	int a=4,*p;
+	cout<<"Here is a variable a";
 	p=&a;
-	cout<<endl<<"The address of variable a is: "<<p;
-	*n=&*p;
-	cout<<endl<<"The address of pointer p is: "<<n;
+	cout<<endl<<"The address of variable a is: "<<p<<endl;
+	*p=a;
+	cout<<"The value of a is: "<<a<<endl;
 	return 0;
 }
