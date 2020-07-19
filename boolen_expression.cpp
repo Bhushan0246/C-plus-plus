b2c71f904ad78067861d44468741063072f91b7d
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2020-07-19 13:11:32 +0530

    Add files via upload

---
+++ boolen_expression.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+bool x,y;
+cout<<"Boolen Greater than.\n"<<endl;
+cout<<"The value of y is: 1";
+cout<<"Enter a value for 'x': ";
+cin>>x;
+cout<<"\nIs x is greater then y: "<<(x>y);
+return 0;
+}
+
