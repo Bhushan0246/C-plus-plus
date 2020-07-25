11dd07bced3f40c73d3ba2351ad4b70787c56a20
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2020-07-25 15:11:14 +0530

    Add files via upload

---
+++ function_parameter.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void myFunction(string firstname,int age) {
+  cout << firstname << " Harode. "<<age<<" years old.\n";
+}
+
+int main() {
+  myFunction("Bala", 21);
+  myFunction("Jammy", 20);
+  myFunction("Brijo", 19);
+  myFunction("Yogesh", 18);
+  myFunction("Jaggu", 17);
+  myFunction("Bhushan", 17);
+  return 0;
+}
+
